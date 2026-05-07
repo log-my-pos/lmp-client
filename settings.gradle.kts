2eab07564eb7afc63ffd97b8be054166fabc6759
The Panda Oliver <Oliver.h.froberg@gmail.com>
2026-05-07 12:31:47 +0200

    Moved everything to the root project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,33 +1,14 @@
-rootProject.name = "LogMyPos-Client"
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-
 pluginManagement {
 	repositories {
-		google {
-			mavenContent {
-				includeGroupAndSubgroups("androidx")
-				includeGroupAndSubgroups("com.android")
-				includeGroupAndSubgroups("com.google")
-			}
-		}
+		google()
 		mavenCentral()
 		gradlePluginPortal()
 	}
 }
 
-dependencyResolutionManagement {
-	repositories {
-		google {
-			mavenContent {
-				includeGroupAndSubgroups("androidx")
-				includeGroupAndSubgroups("com.android")
-				includeGroupAndSubgroups("com.google")
-				includeGroupAndSubgroups("org.chromium")
-			}
-		}
-		mavenCentral()
-		maven("https://api.mapbox.com/downloads/v2/releases/maven")
-	}
+plugins {
+	id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
-include(":composeApp")
+rootProject.name = "LogMyPos-Client"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
--- src/androidMain/kotlin/dev/pandasystems/logmypos_client/MainActivity.kt
+++ src/androidMain/kotlin/dev/pandasystems/logmypos_client/MainActivity.kt
@@ -1,9 +1,7 @@
 package dev.pandasystems.logmypos_client
 
-import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
