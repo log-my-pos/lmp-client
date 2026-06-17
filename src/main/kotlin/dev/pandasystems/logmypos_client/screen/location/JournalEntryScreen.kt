4e16085140625f615015c5e00b2ca7b53771fdd2
The Panda Oliver <Oliver.h.froberg@gmail.com>
2026-06-17 18:56:25 +0200

    feat: add default image handling for empty journal entries in `JournalEntryScreen`, optimize image loading with `AsyncImage`, and improve haze effect and UI alignment

--- src/main/kotlin/dev/pandasystems/logmypos_client/screen/location/JournalEntryScreen.kt
+++ src/main/kotlin/dev/pandasystems/logmypos_client/screen/location/JournalEntryScreen.kt
@@ -1,35 +1,33 @@
 package dev.pandasystems.logmypos_client.screen.location
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import coil3.compose.AsyncImage
+import com.composables.icons.tabler.Tabler
+import com.composables.icons.tabler.outline.PhotoX
 import dev.chrisbanes.haze.blur.blurEffect
 import dev.chrisbanes.haze.blur.materials.CupertinoMaterials
 import dev.chrisbanes.haze.hazeEffect
 import dev.chrisbanes.haze.hazeSource
 import dev.chrisbanes.haze.rememberHazeState
-import dev.pandasystems.logmypos_client.R
 import dev.pandasystems.logmypos_client.data.JournalEntry
 import dev.pandasystems.logmypos_client.repository.JournalRepository
 import dev.pandasystems.logmypos_client.theme.Colors
@@ -73,45 +71,52 @@ class JournalEntryScreen(
 						Modifier
 							.fillMaxSize()
 					) {
-						AsyncImage(
-							model = if (isPreview) {
-								R.drawable.preview_image2
-							} else {
-								"https://picsum.photos/300/300"
-							},
-							contentDescription = null,
-							modifier = Modifier
-                                .fillMaxSize()
-                                .hazeSource(hazeState),
-							contentScale = ContentScale.Crop
-						)
-
-						val blurStyle = CupertinoMaterials.ultraThin(Color(0x66272726))
-						val shape = CircleShape
-
-						Box(
-							Modifier
-                                .padding(8.dp)
-                                .align(Alignment.BottomStart)
-                                .clip(shape)
-                                .hazeEffect(hazeState) {
-                                    blurEffect {
-                                        style = blurStyle
-                                    }
-                                }
-                                .padding(8.dp, 4.dp)
-						) {
-							Text(
-								entry!!.imagePaths.toString(),
-								color = Color(0xFFFFFFFF),
+						if (entry!!.imagePaths.isEmpty()) {
+							Image(
+								Tabler.Outline.PhotoX,
+								contentDescription = null,
+								modifier = Modifier
+									.align(Alignment.Center)
+									.size(128.dp),
+								colorFilter = ColorFilter.tint(Colors.text.copy(alpha = 0.2f))
 							)
+						} else {
+							AsyncImage(
+								model = entry!!.imagePaths.first(),
+								contentDescription = null,
+								modifier = Modifier
+									.fillMaxSize()
+									.hazeSource(hazeState),
+								contentScale = ContentScale.Crop
+							)
+
+							val blurStyle = CupertinoMaterials.ultraThin(Color(0x66272726))
+							val shape = CircleShape
+
+							Box(
+								Modifier
+									.padding(8.dp)
+									.align(Alignment.BottomStart)
+									.clip(shape)
+									.hazeEffect(hazeState) {
+										blurEffect {
+											style = blurStyle
+										}
+									}
+									.padding(8.dp, 4.dp)
+							) {
+								Text(
+									entry!!.imagePaths.size.toString(),
+									color = Color(0xFFFFFFFF),
+								)
+							}
 						}
 					}
 				}
 			}
 			Card(9.54f) {
 				if (isLoading) {
-                    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {CircularProgressIndicator()}
+					Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) { CircularProgressIndicator() }
 				} else {
 				}
 			}
@@ -122,8 +127,8 @@ class JournalEntryScreen(
 	private fun Card(rotation: Float, content: @Composable () -> Unit) {
 		Surface(
 			Modifier
-                .size(240.dp)
-                .rotate(rotation),
+				.size(240.dp)
+				.rotate(rotation),
 			shape = RoundedCornerShape(24.dp),
 			color = Colors.backgroundSecondary,
 			border = BorderStroke(4.dp, Colors.borderColor),
@@ -131,8 +136,8 @@ class JournalEntryScreen(
 		) {
 			Box(
 				Modifier
-                    .fillMaxSize()
-                    .padding(4.dp)
+					.fillMaxSize()
+					.padding(4.dp)
 			) {
 				content()
 			}
